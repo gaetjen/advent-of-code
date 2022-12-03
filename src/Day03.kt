e047990eff6455e2a8afb4ec7a2afca2b0dbf3c6
Johannes Gätjen <joga@monta.com>
2022-12-03 09:16:50 +0100

    solve day 3

---
+++ src/Day03.kt
@@ -0,0 +1,37 @@
+object Day03 {
+    fun part1(input: List<String>): Int {
+        return input.sumOf { wrongItem(it) }
+    }
+
+    private fun wrongItem(input: String): Int {
+        val (c1, c2) = input.chunked(input.length / 2) {
+            it.toSet()
+        }
+        val wrongItem = c1.intersect(c2).first()
+        return itemToPriority(wrongItem)
+    }
+
+    private fun itemToPriority(item: Char) = if (item in ('a'..'z')) {
+        item.code - 'a'.code + 1
+    } else {
+        item.code - 'A'.code + 27
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.chunked(3).sumOf { badge(it) }
+    }
+
+    private fun badge(items: List<String>) :Int {
+        return items
+            .map { it.toSet() }
+            .reduce { acc, chars -> acc.intersect(chars) }
+            .first()
+            .let { itemToPriority(it) }
+    }
+}
+
+fun main() {
+    val input = readInput("resources/day03")
+    println(Day03.part1(input))
+    println(Day03.part2(input))
+}
