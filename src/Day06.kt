15581cc8121205548d7ee120664279051c2c3b3d
Johannes Gätjen <joga@monta.com>
2022-12-06 06:19:46 +0100

    extract function day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,31 +2,23 @@ import kotlin.math.max
 
 object Day06 {
     fun part1(input: String): Int {
-        return input
-            .mapIndexed { index, _ ->
-                index + 1 to
-                        input
-                            .drop(max(0, index - 3))
-                            .take(4)
-                            .toSet()
-                            .count()
-            }
-            .first { (_, distinct) -> distinct == 4 }
-            .first
+        return numberOfCharsBeforeNDistinct(input, 4)
     }
 
+    private fun numberOfCharsBeforeNDistinct(input: String, n: Int) = input
+        .mapIndexed { index, _ ->
+            index + 1 to
+                    input
+                        .drop(max(0, index - n + 1))
+                        .take(n)
+                        .toSet()
+                        .count()
+        }
+        .first { (_, distinct) -> distinct == n }
+        .first
+
     fun part2(input: String): Int {
-        return input
-            .mapIndexed { index, _ ->
-                index + 1 to
-                        input
-                            .drop(max(0, index - 13))
-                            .take(14)
-                            .toSet()
-                            .count()
-            }
-            .first { (_, distinct) -> distinct == 14 }
-            .first
+        return numberOfCharsBeforeNDistinct(input, 14)
     }
 
 }
