e6a4223c1943655f840219424cfa4e204f573174
Johannes Gätjen <joga@monta.com>
2022-12-07 07:14:29 +0100

    solve day 7

---
+++ src/Day07.kt
@@ -0,0 +1,139 @@
+object Day07 {
+    data class File(
+        val size: Long?,
+        val parent: File? = null,
+        val children: MutableList<File> = mutableListOf(),
+        val name: String
+    ) {
+        fun totalSize() : Long {
+            if (children.size == 0) {
+                return size ?: 0
+            }
+            return children.sumOf { it.totalSize() }
+        }
+        fun path() : String {
+            return if (parent == null) {
+                name
+            } else {
+                parent.path() + "/" + name
+            }
+        }
+    }
+
+    fun part1(input: List<String>): Long {
+        val allDirs = dirsFromInput(input)
+        println(allDirs.map { it.totalSize() })
+        println(allDirs.all { it.size == null })
+        println(allDirs.size)
+        println(input.filter { it.startsWith("dir ") }.size)
+        /*for (line in input) {
+            if (line.startsWith("dir")) {
+                if (line.split(" ").last() !in allDirs.keys) {
+                    println("!!! $line")
+                    break
+                }
+            }
+        }*/
+        return allDirs.map { it.totalSize() }.filter { it <= 100000}.sum()
+    }
+
+    private fun dirsFromInput(input: List<String>): MutableList<File> {
+        val root = File(null, name = "")
+        var currentDir = root
+        val allDirs = mutableListOf(root)
+        var idx = 0
+        while (idx < input.size) {
+            //println(input[idx])
+            when (input[idx]) {
+                "$ cd .." -> {
+                    currentDir = currentDir.parent!!
+                    idx++
+                }
+
+                "$ ls" -> idx = processLs(input, idx, allDirs, currentDir)
+                else -> { // $ cd <dir>
+                    currentDir = currentDir.children.first { it.name == input[idx].split(" ").last() }
+                    idx++
+                }
+            }
+        }
+        return allDirs
+    }
+
+    private fun processLs(input: List<String>, idx: Int, allDirs: MutableList<File>, thisDir: File): Int {
+        var rtnIdx = idx + 1
+        while (rtnIdx < input.size && input[rtnIdx][0] != '$') {
+            val (p1, p2) = input[rtnIdx].split(" ")
+            when (p1) {
+                "dir" -> {
+                    val newDir = File(null, thisDir, name = p2)
+                    allDirs.add(newDir)
+                    thisDir.children.add(newDir)
+                }
+                else -> {
+                    val newFile = File(p1.toLong(), name = p2)
+                    thisDir.children.add(newFile)
+                }
+            }
+            rtnIdx++
+        }
+        return rtnIdx
+    }
+
+    fun part2(input: List<String>): Long {
+        val allDirs = dirsFromInput(input)
+        val totalMem = allDirs[0].totalSize()
+        println(allDirs[0].path())
+        val freeSpace = 70000000 - totalMem
+        println("free: $freeSpace")
+        val toFree = 30000000 - freeSpace
+        println("need to free $toFree")
+        return allDirs
+            .filter { it.totalSize() >= toFree }
+            .minBy { it.totalSize() }
+            .totalSize()
+    }
+}
+
+val testInput = """
+    ${'$'} cd /
+    ${'$'} ls
+    dir a
+    14848514 b.txt
+    8504156 c.dat
+    dir d
+    ${'$'} cd a
+    ${'$'} ls
+    dir e
+    29116 f
+    2557 g
+    62596 h.lst
+    ${'$'} cd e
+    ${'$'} ls
+    584 i
+    ${'$'} cd ..
+    ${'$'} cd ..
+    ${'$'} cd d
+    ${'$'} ls
+    4060174 j
+    8033020 d.log
+    5626152 d.ext
+    7214296 k
+""".trimIndent()
+
+/*val testInput = """
+    ${'$'} cd /
+    ${'$'} ls
+    dir a
+    14848514 b.txt
+    8504156 c.dat
+""".trimIndent()
+ */
+fun main() {
+    println(Day07.part1(testInput.split("\n").drop(1)))
+    // wrong: 1081027
+    val input = readInput("resources/day07")
+    println(Day07.part1(input.drop(1)))
+    println(Day07.part2(input.drop(1)))
+    //println(Day07.part2(input))
+}
