79f8317c14eeeb3bcb807a9fd899b6f1cb2921ab
Johannes Gätjen <joga@monta.com>
2022-12-13 07:48:15 +0100

    solve day 13. should've used python

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,22 +1,121 @@
 object Day13 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<List<ListItem>> {
+        return input.split { it.isEmpty() }
+            .map {
+                it.map { ln ->
+                    ListItem.of(ln)
+                }
+            }
+    }
+
+    sealed class ListItem {
+        companion object {
+            fun of(string: String): ListItem {
+                if (string.count { it == '[' } != string.count { it == ']' }) {
+                    error("opening and closing brackets don't match")
+                }
+                if ('[' !in string) {
+                    if (',' in string) error("unexpected comma!")
+                    return ListNumber(string.toInt())
+                }
+                val elements = splitOnTopLevelComma(string.drop(1).dropLast(1))
+                return SubList(elements.map { of(it) })
+            }
+        }
+
+        fun compareTo(other: ListItem): Int {
+            return when {
+                this is ListNumber && other is ListNumber -> this.number - other.number
+                this is ListNumber && other is SubList ->  SubList(listOf(this)).compareTo(other)
+                this is SubList && other is ListNumber -> this.compareTo(SubList(listOf(other)))
+                this is SubList && other is SubList -> {
+                    this.list.zip(other.list).map { (ti, oi) ->
+                        ti.compareTo(oi)
+                    }.firstOrNull { it != 0} ?: (this.list.size - other.list.size)
+                }
+                else -> error("exhaustive conditions")
+            }
+        }
+
+
+        data class ListNumber(val number: Int) : ListItem()
+        data class SubList(val list: List<ListItem>) : ListItem()
+    }
+
+    private fun splitOnTopLevelComma(str: String): List<String> {
+        if (str.isEmpty()) return listOf()
+        return if (str[0] == '[') {
+            val idx = getIndexOfMatchingClosing(str)
+            listOf(str.substring(0..idx)) + splitOnTopLevelComma(str.drop(idx + 2))
+        } else {
+            val h = str.takeWhile { it != ',' }
+            val t = str.dropWhile { it != ',' }.drop(1)
+            listOf(h) + splitOnTopLevelComma(t)
+        }
+    }
+
+    private fun getIndexOfMatchingClosing(str: String): Int {
+        var idx = 0
+        var stackLevel = 1
+        while (stackLevel != 0) {
+            idx++
+            when (str[idx]) {
+                '[' -> stackLevel++
+                ']' -> stackLevel--
+                //else -> just continue iterating
+            }
+        }
+        return idx
     }
 
     fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+        val items = parse(input)
+        return items.mapIndexed { index, listItems ->
+            if (listItems[0].compareTo(listItems[1]) <= 0) {
+                index + 1
+            } else {
+                -1
+            }
+        }.filter { it >= 0 }.sum().toLong()
     }
 
     fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+        val cmp = Comparator<ListItem> { o1, o2 ->
+            if (o1 == null || o2 == null) error("did not expect to compare nulls")
+            o1.compareTo(o2)
+        }
+        val items = parse(input).flatten()
+        val (div1, div2) = parse(listOf("[[2]]", "[[6]]")).flatten()
+        val sorted = (items + listOf(div1, div2)).sortedWith(cmp)
+        return (sorted.indexOf(div1) + 1L) * (sorted.indexOf(div2) + 1)
     }
 }
 
 fun main() {
     val testInput = """
-        
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+
+        [[1],[2,3,4]]
+        [[1],4]
+
+        [9]
+        [[8,7,6]]
+
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+
+        [7,7,7,7]
+        [7,7,7]
+
+        []
+        [3]
+
+        [[[]]]
+        [[]]
+
+        [1,[2,[3,[4,[5,6,7]]]],8,9]
+        [1,[2,[3,[4,[5,6,0]]]],8,9]
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day13.part1(testInput))
