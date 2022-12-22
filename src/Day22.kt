5e8ec2d498a9995bd9bb0c1ab26a35ad3fa36c15
Johannes Gätjen <joga@monta.com>
2022-12-22 21:05:35 +0100

    solve day 22 part 2

--- src/Day22.kt
+++ src/Day22.kt
@@ -15,8 +15,8 @@ object Day22 {
         return distances.zip(turns) { a, b -> listOf(a, b) }.flatten() + distances.last()
     }
 
-    sealed class MapTile(val pos: Pos, val real: Boolean) {
-        class Floor(pos: Pos) : MapTile(pos, true)
+    sealed class MapTile(open val pos: Pos, val real: Boolean) {
+        data class Floor(override val pos: Pos) : MapTile(pos, true)
         class Wall(pos: Pos) : MapTile(pos, true)
         class Empty(pos: Pos) : MapTile(pos, false)
     }
@@ -55,7 +55,7 @@ object Day22 {
         UP -> 3
     }
 
-    abstract class Map(val tiles: List<List<MapTile>>) {
+    abstract class MonkeyMap(val tiles: List<List<MapTile>>) {
         var pos: Pos = tiles.first().first { it.real }.pos
         var dir: Direction = RIGHT
 
@@ -70,7 +70,7 @@ object Day22 {
 
         fun moveOne() {
             val newPos = constrain(move(dir, pos))
-            val (newTile, newDir) = realTile(newPos, dir)
+            val (newTile, newDir) = realTile(newPos)
             if (newTile is MapTile.Floor) {
                 pos = newTile.pos
                 dir = newDir
@@ -96,7 +96,7 @@ object Day22 {
             }
         }
 
-        abstract fun realTile(at: Pos, dir: Direction): Pair<MapTile, Direction>
+        abstract fun realTile(at: Pos): Pair<MapTile, Direction>
 
         fun constrain(pos: Pos): Pos {
             return pos.first.mod(tiles.size) to pos.second.mod(tiles.first().size)
@@ -104,13 +104,13 @@ object Day22 {
 
     }
 
-    class FlatMap(tiles: List<List<MapTile>>) : Map(tiles) {
+    class FlatMap(tiles: List<List<MapTile>>) : MonkeyMap(tiles) {
         val leftEdge: List<MapTile> = tiles.map { it.first { tile -> tile.real } }
         val rightEdge: List<MapTile> = tiles.map { it.last { tile -> tile.real } }
         val topEdge: List<MapTile> = transpose(tiles).map { it.first { tile -> tile.real } }
         val bottomEdge: List<MapTile> = transpose(tiles).map { it.last { tile -> tile.real } }
 
-        override fun realTile(at: Pos, dir: Direction): Pair<MapTile, Direction> {
+        override fun realTile(at: Pos): Pair<MapTile, Direction> {
             return if (tiles[at].real) {
                 tiles[at] to dir
             } else {
@@ -124,24 +124,30 @@ object Day22 {
         }
     }
 
-    class CubeMap(tiles: List<List<MapTile>>) : Map(tiles) {
-        override fun realTile(at: Pos, dir: Direction): Pair<MapTile, Direction> {
+    class CubeMap(tiles: List<List<MapTile>>) : MonkeyMap(tiles) {
+        override fun realTile(at: Pos): Pair<MapTile, Direction> {
             if (tiles[at].real) {
                 return tiles[at] to dir
             }
             return when {
-                at.first == 50 && at.second >= 100 && dir == DOWN -> tiles[at.second to 99] to LEFT
-                at.first in 50 until 100 && at.second == 100 -> tiles[49 to at.first] to UP
+                at.first == 50 && at.second in 100 until 150 && dir == DOWN -> tiles[at.second - 50 to 99] to LEFT
+                at.first in 50 until 100 && at.second == 100 && dir == RIGHT -> tiles[49 to at.first + 50] to UP
+
                 at.first == 99 && at.second in 0 until 50 && dir == UP -> tiles[50 + at.second to 50] to RIGHT
                 at.first in 50 until 100 && at.second == 49 && dir == LEFT -> tiles[100 to at.first - 50] to DOWN
+
                 at.first == 150 && at.second in 50 until 100 && dir == DOWN -> tiles[at.second + 100 to 49] to LEFT
                 at.first in 150 until 200 && at.second == 50 && dir == RIGHT -> tiles[149 to at.first - 100] to UP
+
                 at.first == 199 && at.second in 50 until 100 && dir == UP -> tiles[at.second + 100 to 0] to RIGHT
                 at.first in 150 until 200 && at.second == 149 && dir == LEFT -> tiles[0 to at.first - 100] to DOWN
+
                 at.first == 199 && at.second in 100 until 150 && dir == UP -> tiles[199 to at.second - 100] to UP
                 at.first == 0 && at.second in 0 until 50 && dir == DOWN -> tiles[0 to at.second + 100] to DOWN
+
                 at.first in 0 until 50 && at.second == 0 && dir == RIGHT -> tiles[149 - at.first to 99] to LEFT
                 at.first in 100 until 150 && at.second == 100 && dir == RIGHT -> tiles[149 - at.first to 149] to LEFT
+
                 at.first in 0 until 50 && at.second == 49 && dir == LEFT -> tiles[149 - at.first to 0] to RIGHT
                 at.first in 100 until 150 && at.second == 149 && dir == LEFT -> tiles[149 - at.first to 50] to RIGHT
 
@@ -150,16 +156,60 @@ object Day22 {
         }
     }
 
+    fun debug(input: List<String>) {
+        val (mapInput, _) = input.split { it.isEmpty() }
+        val tiles = parseMap(mapInput)
+        val map = CubeMap(tiles)
+        val flat = FlatMap(tiles)
+        val edges = listOf(flat.topEdge, flat.rightEdge, flat.bottomEdge, flat.leftEdge)
+        val landingPoints = Direction.values().zip(edges).map { (d, ts) ->
+            ts.map {
+                map.pos = it.pos
+                map.dir = d
+                map.move(Instruction.Walk(1))
+                map.pos
+            }
+        }
+        println("number points landing: ${landingPoints.flatten().size} / ${landingPoints.flatten().distinct().size}")
+        println("number points edges: ${edges.flatten().size} / ${edges.flatten().distinct().size}")
+        val flatEdges = edges.flatten().map { it.pos }.toSet()
+        val flatLandings = landingPoints.flatten().toSet()
+        printGrid(flatEdges.associateWith { '█' })
+        val edgeWalls = flatEdges.filter { map.tiles[it] is MapTile.Wall }.associateWith { '#' }
+        val landingChars = flatLandings.associateWith { '█' } + edgeWalls
+        printGrid(landingChars)
+    }
+
+
     fun part2(input: List<String>): Long {
         val (mapInput, walkInput) = input.split { it.isEmpty() }
         val tiles = parseMap(mapInput)
         val instructions = parseInstructions(walkInput.first())
         val map = CubeMap(tiles)
         instructions.forEach {
-            //println("${map.pos}, ${map.dir}, $it")
             map.move(it)
         }
+        println("${map.pos} ${map.dir}, (${dirNum(map.dir)})")
         return (map.pos.first + 1) * 1000L + (map.pos.second + 1) * 4 + dirNum(map.dir)
+    }
+
+    private fun printGrid(positions: Map<Pos, Char>) {
+        println("number positions in grid: ${positions.size}")
+        val minRow = positions.keys.minOf { it.first }
+        val maxRow = positions.keys.maxOf { it.first }
+        val minCol = positions.keys.minOf { it.second }
+        val maxCol = positions.keys.maxOf { it.second }
+        val result = List(maxRow - minRow + 1) { rowIdx ->
+            List(maxCol - minCol + 1) { colIdx ->
+                if (rowIdx + minRow to colIdx + minCol in positions) {
+                    positions[rowIdx + minRow to colIdx + minCol]
+                } else {
+                    ' '
+                }
+            }.joinToString("")
+        }
+        println(result.joinToString("\n"))
+        println("------------------------------------------------------------")
     }
 
 }
@@ -191,6 +241,7 @@ fun main() {
 
     println("------Real------")
     val input = readInput("resources/day22")
+    Day22.debug(input)
     println(Day22.part1(input))
     // 13383
     println(Day22.part2(input))
