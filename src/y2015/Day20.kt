298638d394d735cf7d58f7c25971aed9c832d4b9
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-11-09 05:11:10 +0100

    2015 day 20

---
+++ src/y2015/Day20.kt
@@ -0,0 +1,50 @@
+package y2015
+
+import util.readInput
+import kotlin.math.ceil
+import kotlin.math.sqrt
+
+object Day20 {
+    private fun factors(n: Int): List<Int> {
+        return (1..ceil(sqrt(n.toDouble())).toInt()).mapNotNull {
+            if (n % it == 0) listOf(it, n / it) else null
+        }.flatten()
+    }
+
+    fun part1(input: Int): Int {
+        val target = input / 10
+        var x = 1
+        while (true) {
+            if (factors(x).sum() >= target) {
+                return x
+            }
+            x++
+        }
+    }
+
+    private fun factors50(n: Int): List<Int> {
+        return factors(n).filter { 50 * it >= n }
+    }
+
+    fun part2(input: Int): Int {
+        var x = 1
+        while (true) {
+            if (factors50(x).sum() * 11 >= input) {
+                return x
+            }
+            x++
+        }
+    }
+}
+
+fun main() {
+    val testInput = 140
+    println("------Tests------")
+    println(Day20.part1(testInput))
+    println(Day20.part2(testInput))
+
+    println("------Real------")
+    val input = 33100000
+    println(Day20.part1(input))
+    println(Day20.part2(input))
+}
