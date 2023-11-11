b68477e528392c335d65a425eef898d87351b42b
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-11-11 13:56:59 +0100

    2016 day 5

--- src/y2016/Day05.kt
+++ src/y2016/Day05.kt
@@ -1,33 +1,48 @@
 package y2016
 
-import util.readInput
+import util.md5
 
 object Day05 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+
+    fun part1(input: String): String {
+        var index = 0
+        val characters = mutableListOf<Char>()
+        while (characters.size < 8) {
+            val hash = (input + index.toString()).md5()
+            if (hash.startsWith("00000")) {
+                characters.add(hash[5])
+            }
+            index++
+        }
+        return characters.joinToString(separator = "") { it.toString() }
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
-    }
-
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part2(input: String): String {
+        var index = 0
+        val characters = "________".toMutableList()
+        while (characters.contains('_')) {
+            val hash = (input + index.toString()).md5()
+            if (hash.startsWith("00000")) {
+                val pwIndex = hash[5].code - '0'.code
+                if (pwIndex in 0..7 && characters[pwIndex] == '_') {
+                    characters[pwIndex] = hash[6]
+                    println(characters.joinToString(separator = "") { it.toString() })
+                }
+            }
+            index++
+        }
+        return characters.joinToString(separator = "") { it.toString() }
     }
 }
 
 fun main() {
-    val testInput = """
-
-    """.trimIndent().split("\n")
+    val testInput = "abc"
     println("------Tests------")
     println(Day05.part1(testInput))
     println(Day05.part2(testInput))
 
     println("------Real------")
-    val input = readInput("resources/2016/day05")
+    val input = "ojvtpuvg"
     println(Day05.part1(input))
     println(Day05.part2(input))
 }
