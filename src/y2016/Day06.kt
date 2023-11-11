6cbf5b67a182675cc5a08e1578b6f93b7d663f2c
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-11-11 14:02:18 +0100

    2016 day 6

--- src/y2016/Day06.kt
+++ src/y2016/Day06.kt
@@ -1,26 +1,44 @@
 package y2016
 
 import util.readInput
+import util.transpose
 
 object Day06 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    fun part1(input: List<String>): String {
+        return transpose(input.map {
+            it.toList()
+        }).map { chars ->
+            chars.groupingBy { it }.eachCount().maxBy { it.value }.key
+        }.joinToString(separator = "") { it.toString() }
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
-    }
-
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part2(input: List<String>): String {
+        return transpose(input.map {
+            it.toList()
+        }).map { chars ->
+            chars.groupingBy { it }.eachCount().minBy { it.value }.key
+        }.joinToString(separator = "") { it.toString() }
     }
 }
 
 fun main() {
     val testInput = """
-
+        eedadn
+        drvtee
+        eandsr
+        raavrd
+        atevrs
+        tsrnev
+        sdttsa
+        rasrtv
+        nssdts
+        ntnada
+        svetve
+        tesnvt
+        vntsnd
+        vrdear
+        dvrsen
+        enarar
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day06.part1(testInput))
