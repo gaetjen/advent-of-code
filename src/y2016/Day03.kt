978f125ee515eec265faf47901358913d45b5980
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-11-11 13:15:09 +0100

    2016 day 3

--- src/y2016/Day03.kt
+++ src/y2016/Day03.kt
@@ -1,26 +1,45 @@
 package y2016
 
 import util.readInput
+import util.transpose
 
 object Day03 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<List<Int>> {
+        return input.map { line ->
+            line.trim().split(Regex("\\s+")).map {
+                it.toInt()
+            }
+        }
     }
 
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>): Int {
         val parsed = parse(input)
-        return 0L
+        return parsed.count {
+            it.sum() - it.max() > it.max()
+        }
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(input: List<String>): Int {
         val parsed = parse(input)
-        return 0L
+        return parsed.chunked(3).map {
+            transpose(it)
+        }.flatten().count {
+            it.sum() - it.max() > it.max()
+        }
     }
 }
 
 fun main() {
     val testInput = """
-
+        5 10 25
+        5 10 25
+        2 2 3
+        101 301 501
+        102 302 502
+        103 303 503
+        201 401 601
+        202 402 602
+        203 403 603
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day03.part1(testInput))
