3b842a20f7bc4791423686c7c3dc3214b309848d
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-11-19 22:38:46 +0100

    2016 day 15

--- src/y2016/Day15.kt
+++ src/y2016/Day15.kt
@@ -3,24 +3,46 @@ package y2016
 import util.readInput
 
 object Day15 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<Pair<Int, Int>> {
+        return input.map {
+            val els = it.split(' ')
+            els[3].toInt() to els.last().dropLast(1).toInt()
+        }
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part1(input: List<String>): Int {
+        val initialState = parse(input)
+        val (divisors, remainders) = initialState.mapIndexed { idx, (divisor, rest) ->
+            divisor to (divisor - rest - idx - 1).mod(divisor)
+        }.unzip()
+        var result = 0
+        while (true) {
+            if (divisors.map { result % it } == remainders) {
+                return result
+            }
+            result++
+        }
     }
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part2(input: List<String>): Int {
+        val initialState = parse(input) + listOf(11 to 0)
+        val (divisors, remainders) = initialState.mapIndexed { idx, (divisor, rest) ->
+            divisor to (divisor - rest - idx - 1).mod(divisor)
+        }.unzip()
+        var result = 0
+        while (true) {
+            if (divisors.map { result % it } == remainders) {
+                return result
+            }
+            result++
+        }
     }
 }
 
 fun main() {
     val testInput = """
-
+        Disc #1 has 5 positions; at time=0, it is at position 4.
+        Disc #2 has 2 positions; at time=0, it is at position 1.
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day15.part1(testInput))
