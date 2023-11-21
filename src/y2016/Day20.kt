05192ead564f6421075df93b1e46c0f5afe432cc
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-11-21 23:34:25 +0100

    2016 day 20

--- src/y2016/Day20.kt
+++ src/y2016/Day20.kt
@@ -2,32 +2,59 @@ package y2016
 
 import util.readInput
 
+enum class Indicator {
+    START, STOP
+}
+
 object Day20 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<Pair<Long, Indicator>> {
+        return input.flatMap { line ->
+            val (start, stop) = line.split("-").map { it.toLong() }
+            listOf(start to Indicator.START, stop to Indicator.STOP)
+        }.sortedBy { it.first }
     }
 
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>) {
         val parsed = parse(input)
-        return 0L
+        val stackDepth = parsed.scan(0) { acc, pair ->
+            if (pair.second == Indicator.START) {
+                acc + 1
+            } else {
+                acc - 1
+            }
+        }.drop(1)
+        println(parsed)
+        println(stackDepth)
+        val idx = stackDepth.indexOf(0)
+        println("stop: ${parsed[idx]}")
+        println("next: ${parsed[idx + 1]}")
+        part2(parsed, stackDepth)
     }
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part2(parsed: List<Pair<Long, Indicator>>, stackDepth: List<Int>) {
+        val gaps = stackDepth.dropLast(1)
+            .mapIndexedNotNull { idx, depth ->
+            if (depth != 0) {
+                null
+            } else {
+                parsed[idx + 1].first - parsed[idx].first - 1
+            }
+        }
+        println("gaps: $gaps")
+        println("total: ${gaps.sum()}")
     }
 }
 
 fun main() {
     val testInput = """
-
+        5-8
+        0-2
+        4-7
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day20.part1(testInput))
-    println(Day20.part2(testInput))
 
     println("------Real------")
     val input = readInput("resources/2016/day20")
     println(Day20.part1(input))
-    println(Day20.part2(input))
 }
