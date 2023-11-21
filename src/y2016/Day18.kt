bba2de7c8ab7a225b034254463aaaeaf3c387ca3
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-11-21 23:04:44 +0100

    2016 day 18

--- src/y2016/Day18.kt
+++ src/y2016/Day18.kt
@@ -1,33 +1,37 @@
 package y2016
 
-import util.readInput
-
 object Day18 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    fun part1(input: String, rows: Int): Int {
+        val map = mutableListOf(input)
+        repeat(rows - 1) {
+            map.add(nextRow(map.last()))
+        }
+        return map.sumOf { row -> row.count { it == '.' } }
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
-    }
+    val dangerous = setOf(
+        "^^.",
+        ".^^",
+        "^..",
+        "..^"
+    )
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    private fun nextRow(row: String): String {
+        return ".$row.".windowed(3, 1).joinToString(separator = "") {
+            if (it in dangerous) "^" else "."
+        }
     }
 }
 
 fun main() {
-    val testInput = """
-
-    """.trimIndent().split("\n")
+    val testInput = ".^^.^.^^^^"
+    val testRows = 10
     println("------Tests------")
-    println(Day18.part1(testInput))
-    println(Day18.part2(testInput))
+    println(Day18.part1(testInput, testRows))
 
     println("------Real------")
-    val input = readInput("resources/2016/day18")
-    println(Day18.part1(input))
-    println(Day18.part2(input))
+    val input = ".^^.^^^..^.^..^.^^.^^^^.^^.^^...^..^...^^^..^^...^..^^^^^^..^.^^^..^.^^^^.^^^.^...^^^.^^.^^^.^.^^.^."
+    val rows = 40
+    println(Day18.part1(input, rows))
+    println(Day18.part1(input, 400000))
 }
