9bfb47288b444b5750ed222186d615c3dceaa97c
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-12-06 07:04:26 +0100

    2023 day 6 with optimizations

--- src/y2023/Day06.kt
+++ src/y2023/Day06.kt
@@ -3,6 +3,7 @@ package y2023
 import util.product
 import util.readInput
 import util.timingStatistics
+import kotlin.math.sqrt
 
 object Day06 {
     private fun parse(input: List<String>): Pair<List<Int>, List<Int>> {
@@ -25,17 +26,39 @@ object Day06 {
         }
     }
 
-    fun parse2(input: List<String>): Pair<Long, Long> {
-        val time = input[0].split(Regex("\\s+")).drop(1).joinToString(separator = "").toLong()
-        val distance = input[1].split(Regex("\\s+")).drop(1).joinToString(separator = "").toLong()
-        return time to distance
-    }
-
     fun part2(input: List<String>): Int {
         val (time, distance) = parse2(input)
         return (0..time + 1).count { t ->
             (time - t) * t > distance
         }
+    }
+
+    fun part2Optimized(input: List<String>): Long {
+        val (time, distance) = parse2(input)
+        val firstWin = (0..time + 1).first { t ->
+            (time - t) * t > distance
+        }
+        val lastWin = (time + 1 downTo 0).first { t ->
+            (time - t) * t > distance
+        }
+        return lastWin - firstWin + 1
+    }
+
+    fun parse2(input: List<String>): Pair<Long, Long> {
+        val time = input[0].dropWhile { !it.isDigit() }.replace(" ", "").toLong()
+        val distance = input[1].dropWhile { !it.isDigit() }.replace(" ", "").toLong()
+        return time to distance
+    }
+
+    // distance: D = (T - t) * t
+    // -t² + Tt - D = 0
+    // t = (-T ± sqrt(T² - 4D)) / -2
+    fun part2Optimized2(input: List<String>): Long {
+        val (time, distance) = parse2(input)
+        val root = sqrt(time * time - 4 * distance.toDouble())
+        val t1 = (-time + root) / -2
+        val t2 = (-time - root) / -2
+        return t2.toLong() - t1.toLong()
     }
 }
 
@@ -47,11 +70,17 @@ fun main() {
     println("------Tests------")
     println(Day06.part1(testInput))
     println(Day06.part2(testInput))
+    println(Day06.part2Optimized(testInput))
+    println(Day06.part2Optimized2(testInput))
 
     println("------Real------")
     val input = readInput(2023, 6)
     println("Part 1 result: ${Day06.part1(input)}")
     println("Part 2 result: ${Day06.part2(input)}")
+    println("Part 2 result: ${Day06.part2Optimized(input)}")
+    println("Part 2 result: ${Day06.part2Optimized2(input)}")
     timingStatistics { Day06.part1(input) }
     timingStatistics { Day06.part2(input) }
+    timingStatistics { Day06.part2Optimized(input) }
+    timingStatistics { Day06.part2Optimized2(input) }
 }
