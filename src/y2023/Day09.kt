a2b03bdca00451101cf2cf2ee9cc972d37714057
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-12-09 06:24:34 +0100

    2023 day 9 (including slight cleanup)

--- src/y2023/Day09.kt
+++ src/y2023/Day09.kt
@@ -4,23 +4,51 @@ import util.readInput
 import util.timingStatistics
 
 object Day09 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<List<Long>> {
+        return input.map { line ->
+            line.split(" ").map { it.toLong() }
+        }
     }
 
     fun part1(input: List<String>): Long {
         val parsed = parse(input)
-        return 0L
+        return parsed.sumOf { extrapolated(it) }
+    }
+
+    fun gradient(lst: List<Long>): List<Long> {
+        return lst.zipWithNext { a, b -> b - a }
+    }
+
+    fun allGradients(lst: List<Long>): List<List<Long>> {
+        return buildList {
+            add(lst)
+            while (!last().all { it == 0L }) {
+                add(gradient(last()))
+            }
+        }
+    }
+
+    fun extrapolated(lst: List<Long>): Long {
+        return allGradients(lst).sumOf { it.last() }
     }
 
     fun part2(input: List<String>): Long {
         val parsed = parse(input)
-        return 0L
+        return parsed.sumOf { line ->
+            allGradients(line).map {
+                it.first()
+            }.reversed().reduce { acc, b ->
+                b - acc
+            }
+        }
     }
 }
 
 fun main() {
     val testInput = """
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day09.part1(testInput))
