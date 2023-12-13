1a474649975aef5cf3c6fbdc0c89fe7a66bc67d6
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-12-13 07:50:40 +0100

    2023 day 13

--- src/y2023/Day13.kt
+++ src/y2023/Day13.kt
@@ -1,26 +1,95 @@
 package y2023
 
 import util.readInput
+import util.split
 import util.timingStatistics
+import util.transpose
 
 object Day13 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<List<String>> {
+        return input.split { it.isBlank() }
     }
 
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>): Int {
         val parsed = parse(input)
-        return 0L
+        return parsed.sumOf { block ->
+            val row = reflectionRow(block)
+            val col = reflectionRow(transpose(block.map { it.toList() }))
+            if (row == null && col != null) {
+                col
+            } else if (col == null && row != null) {
+                row * 100
+            } else if (col != null && row != null) {
+                println("found both: $col, $row")
+                col
+            } else {
+                error("no solution found")
+            }
+        }
     }
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun <T> reflectionRow(rows: List<T>): Int? {
+        val candidates = rows.zipWithNext().mapIndexedNotNull { idx, (row1, row2) ->
+            if (row1 == row2) idx else null
+        }
+        return candidates.firstNotNullOfOrNull { verify(it + 1, rows) }
     }
+
+    fun <T> verify(rowNumber: Int, rows: List<T>): Int? {
+        val zip = rows.take(rowNumber).reversed().zip(rows.drop(rowNumber))
+        return if (zip.all { (row1, row2) -> row1 == row2 }) rowNumber else null
+    }
+
+    fun part2(input: List<String>): Int {
+        val parsed = parse(input)
+        return parsed.sumOf { block ->
+            val chars = block.map { it.toList() }
+            val row = offByOneReflectionRow(chars)
+            val col = offByOneReflectionRow(transpose(chars))
+            if (row == null && col != null) {
+                col
+            } else if (col == null && row != null) {
+                row * 100
+            } else if (col != null && row != null) {
+                println("found both: $col, $row")
+                col
+            } else {
+                error("no solution found")
+            }
+        }
+    }
+
+    fun offByOneReflectionRow(rows: List<List<Char>>): Int? {
+        return rows.indices.firstOrNull { idx ->
+            countErrors(rows.take(idx).reversed().zip(rows.drop(idx))) == 1
+        }
+    }
+
+    private fun countErrors(zip: List<Pair<List<Char>, List<Char>>>): Int {
+        return zip.sumOf { (row1, row2) ->
+            row1.zip(row2).count { (c1, c2) -> c1 != c2 }
+        }
+    }
+
 }
 
 fun main() {
     val testInput = """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day13.part1(testInput))
