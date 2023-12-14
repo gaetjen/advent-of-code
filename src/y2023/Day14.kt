09cf64973dd1cc98ab988f7ad5dcff42987a26a0
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-12-14 07:07:12 +0100

    2023 day 14

--- src/y2023/Day14.kt
+++ src/y2023/Day14.kt
@@ -1,26 +1,132 @@
 package y2023
 
+import util.Cardinal
+import util.Pos
+import util.get
 import util.readInput
+import util.set
 import util.timingStatistics
+import util.transpose
 
 object Day14 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    enum class TileType(val c: Char) {
+        ROLLER('O'), FIXED('#'), EMPTY('.')
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    data class Tile(
+        val type: TileType,
+        var pos: Pos,
+    )
+
+    private fun parse(input: List<String>): MutableList<MutableList<Tile>> {
+        return input.mapIndexed { row, s ->
+            s.mapIndexedNotNull { col, c ->
+                when (c) {
+                    '#' -> {
+                        Tile(
+                            TileType.FIXED,
+                            row to col
+                        )
+                    }
+
+                    'O' -> {
+                        Tile(
+                            TileType.ROLLER,
+                            row to col
+                        )
+                    }
+
+                    '.' -> {
+                        Tile(
+                            TileType.EMPTY,
+                            row to col
+                        )
+                    }
+
+                    else -> {
+                        null
+                    }
+                }
+            }.toMutableList()
+        }.toMutableList()
     }
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part1(input: List<String>): Int {
+        val rocks = parse(input)
+        rocks.flatten().forEach {
+            if (it.type == TileType.ROLLER) {
+                roll(it, Cardinal.NORTH, rocks)
+            }
+        }
+        return rocks.transpose().sumOf {
+            load(it)
+        }
+    }
+
+    fun load(row: List<Tile>): Int {
+        return row.indices.reversed().zip(row).filter { it.second.type == TileType.ROLLER }.sumOf { it.first + 1 }
+    }
+
+    fun Pos.coerce(maxes: Pos): Pos = first.coerceIn(0 until maxes.first) to second.coerceIn(0 until maxes.second)
+
+    fun roll(tile: Tile, dir: Cardinal, rocks: MutableList<MutableList<Tile>>) {
+        val newPos = dir.of(tile.pos).coerce(rocks.size to rocks[0].size)
+        if (rocks[newPos].type != TileType.EMPTY) {
+            return
+        }
+        rocks[newPos] = tile
+        rocks[tile.pos] = Tile(TileType.EMPTY, tile.pos)
+        tile.pos = newPos
+        roll(tile, dir, rocks)
+    }
+
+
+    fun part2(input: List<String>): Int {
+        val rocks = parse(input)
+        val cycle = listOf(
+            Cardinal.NORTH to { it: List<Tile> -> it.sortedBy { it.pos.first } },
+            Cardinal.WEST to { it: List<Tile> -> it.sortedBy { it.pos.second } },
+            Cardinal.SOUTH to { it: List<Tile> -> it.sortedByDescending { it.pos.first } },
+            Cardinal.EAST to { it: List<Tile> -> it.sortedByDescending { it.pos.second } },
+        )
+        val stateHistory = mutableListOf(
+            rocksToString(rocks) to rocks.transpose().sumOf {
+                load(it)
+            }
+        )
+        while (stateHistory.last() !in stateHistory.subList(0, stateHistory.size - 1)) {
+            cycle.forEach { (dir, sort) ->
+                sort(rocks.flatten().filter { it.type == TileType.ROLLER }).forEach {
+                    roll(it, dir, rocks)
+                }
+            }
+            stateHistory.add(rocksToString(rocks) to rocks.transpose().sumOf {
+                load(it)
+            })
+        }
+        val cycleStart = stateHistory.indexOfFirst { it == stateHistory.last() }
+        val cycleLength = stateHistory.size - cycleStart - 1
+        val targetIdx = (1_000_000_000 - cycleStart) % cycleLength + cycleStart
+        return stateHistory[targetIdx].second
+    }
+
+    private fun rocksToString(rocks: MutableList<MutableList<Tile>>) = rocks.joinToString("\n") { row ->
+        row.joinToString("") { it.type.c.toString() }
     }
 }
 
 fun main() {
     val testInput = """
+        O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day14.part1(testInput))
