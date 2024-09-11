2bccd1f3bc220ae546055242580ffa08ba1c7951
Johannes Gätjen <johannesgaetjen@gmail.com>
2024-09-11 20:41:46 +0200

    2017 day 2

--- src/y2017/Day02.kt
+++ src/y2017/Day02.kt
@@ -2,25 +2,38 @@ package y2017
 
 import util.readInput
 import util.timingStatistics
+import kotlin.math.abs
 
 object Day02 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<List<Long>> {
+        return input.map { line ->
+            line.split(Regex("\\s")).map { it.toLong() }
+        }
     }
 
     fun part1(input: List<String>): Long {
         val parsed = parse(input)
-        return 0L
+        val maxes = parsed.map { it.max() }
+        val mins = parsed.map { it.min() }
+        return maxes.zip(mins).map { (a, b) -> abs(a - b) }.sum()
     }
 
     fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+        val parsed = parse(input).map { ln ->
+            val line = ln.sortedDescending()
+            line.firstNotNullOf { first ->
+                line.firstOrNull {first % it == 0L && first != it}?.let { first / it }
+            }
+        }
+        return parsed.sum()
     }
 }
 
 fun main() {
     val testInput = """
+        5 9 2 8
+        9 4 7 3
+        3 8 6 5
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day02.part1(testInput))
