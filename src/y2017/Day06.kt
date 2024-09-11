1cf2c28fc4a54fc5efeb5181060ded4a41b467d0
Johannes Gätjen <johannesgaetjen@gmail.com>
2024-09-11 22:22:20 +0200

    2017 day 6

--- src/y2017/Day06.kt
+++ src/y2017/Day06.kt
@@ -4,23 +4,56 @@ import util.readInput
 import util.timingStatistics
 
 object Day06 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): MutableList<Int> {
+        return input.first().split(Regex("\\s")).map { it.toInt() }.toMutableList()
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part1(input: List<String>): Int {
+        val banks = parse(input)
+        var steps = 0
+        val knownStates = mutableSetOf<List<Int>>()
+        while (true) {
+            if (banks in knownStates) {
+                return steps
+            }
+            knownStates.add(banks.toList())
+            var toDistribute = banks.max()
+            var idx = banks.indexOf(toDistribute)
+            banks[idx] = 0
+            while (toDistribute > 0) {
+                idx = (idx + 1) % banks.size
+                banks[idx]++
+                toDistribute--
+            }
+            steps++
+        }
     }
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part2(input: List<String>): Int {
+        val banks = parse(input)
+        var steps = 0
+        val knownStates = mutableMapOf<List<Int>, Int>()
+        while (true) {
+            if (banks in knownStates) {
+                return steps - knownStates[banks]!!
+            }
+            knownStates[banks] = steps
+            var toDistribute = banks.max()
+            var idx = banks.indexOf(toDistribute)
+            banks[idx] = 0
+            while (toDistribute > 0) {
+                idx = (idx + 1) % banks.size
+                banks[idx]++
+                toDistribute--
+            }
+            steps++
+        }
     }
 }
 
 fun main() {
     val testInput = """
+        0 2 7 0
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day06.part1(testInput))
