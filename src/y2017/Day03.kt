563e6457bbbf63ce0f4934667cf9cbe059a387d1
Johannes Gätjen <johannesgaetjen@gmail.com>
2024-09-11 21:46:47 +0200

    2017 day 3

--- src/y2017/Day03.kt
+++ src/y2017/Day03.kt
@@ -1,33 +1,74 @@
 package y2017
 
-import util.readInput
+import util.Direction
+import util.neighbors
 import util.timingStatistics
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 object Day03 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    fun part1(input: Int): Int {
+        var previousSquareRoot = floor(sqrt(input.toDouble())).toInt()
+        if (previousSquareRoot % 2 == 0) previousSquareRoot--
+        val previousSquare = previousSquareRoot * previousSquareRoot
+        val distanceFromCorner = input - previousSquare
+        val sideStep = (previousSquareRoot + 1) / 2
+        val howFarAlongHalfSide = distanceFromCorner % sideStep
+        val distanceFromMidPoint = if ((distanceFromCorner / sideStep) % 2 == 0) sideStep - howFarAlongHalfSide else howFarAlongHalfSide
+        return ((previousSquareRoot + 1) / 2) + distanceFromMidPoint
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    val spiralPath = sequence {
+        var sideLength = 0
+        var currentPos = 0 to 0
+        yield(currentPos)
+        while (true) {
+            sideLength += 2
+            currentPos = Direction.RIGHT.move(currentPos)
+            yield(currentPos)
+            repeat(sideLength - 1) {
+                currentPos = Direction.UP.move(currentPos)
+                yield(currentPos)
+            }
+            repeat(sideLength) {
+                currentPos = Direction.LEFT.move(currentPos)
+                yield(currentPos)
+            }
+            repeat(sideLength) {
+                currentPos = Direction.DOWN.move(currentPos)
+                yield(currentPos)
+            }
+            repeat(sideLength) {
+                currentPos = Direction.RIGHT.move(currentPos)
+                yield(currentPos)
+            }
+        }
     }
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+
+    fun part2(input: Int): Int {
+        val grid = mutableMapOf((0 to 0) to 1)
+        return spiralPath.drop(1).firstNotNullOf { pos ->
+            val total = pos.neighbors().mapNotNull { grid[it] }.sum()
+            if (total > input) {
+                total
+            } else {
+                grid[pos] = total
+                null
+            }
+        }
     }
+
 }
 
 fun main() {
-    val testInput = """
-    """.trimIndent().split("\n")
+    val testInput = 1024
     println("------Tests------")
     println(Day03.part1(testInput))
     println(Day03.part2(testInput))
 
     println("------Real------")
-    val input = readInput(2017, 3)
+    val input = 277678
     println("Part 1 result: ${Day03.part1(input)}")
     println("Part 2 result: ${Day03.part2(input)}")
     timingStatistics { Day03.part1(input) }
