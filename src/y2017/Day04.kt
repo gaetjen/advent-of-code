80443fd62b9c530d6a121ecebd324e8d3018205d
Johannes Gätjen <johannesgaetjen@gmail.com>
2024-09-11 21:55:41 +0200

    2017 day 4

--- src/y2017/Day04.kt
+++ src/y2017/Day04.kt
@@ -4,23 +4,34 @@ import util.readInput
 import util.timingStatistics
 
 object Day04 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<List<String>> {
+        return input.map {
+            it.split(" ")
+        }
     }
 
     fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+        val phrases = parse(input)
+        return phrases.count {
+            it.distinct().size == it.size
+        }.toLong()
     }
 
     fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+        val phrases = parse(input).map { words ->
+            words.map { w -> w.toCharArray().sorted().toString() }
+        }
+        return phrases.count {
+            it.distinct().size == it.size
+        }.toLong()
     }
 }
 
 fun main() {
     val testInput = """
+        aa bb cc dd ee
+        aa bb cc dd aa
+        aa bb cc dd aaa
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day04.part1(testInput))
