ead0ab644dbe7ed47fd47d72663a6ebc37cefdd1
Johannes Gätjen <johannesgaetjen@gmail.com>
2024-09-20 00:04:00 +0200

    2017 day 15

--- src/y2017/Day15.kt
+++ src/y2017/Day15.kt
@@ -1,33 +1,62 @@
 package y2017
 
-import util.readInput
+import util.PosL
 import util.timingStatistics
+import kotlin.experimental.xor
 
 object Day15 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    val factorA = 16807
+    val factorB = 48271
+
+    fun generate(
+        start: Long,
+        factor: Int
+    ) = sequence {
+        var current = start
+        while (true) {
+            current = (current * factor) % 2_147_483_647
+            yield(current)
+        }
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun generatePart2(
+        start: Long,
+        factor: Int,
+        divisible: Int
+    ) = sequence {
+        var current = start
+        while (true) {
+            current = (current * factor) % 2_147_483_647
+            if ((current % divisible) == 0L) yield(current)
+        }
     }
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+
+    fun part1(input: PosL): Int {
+        return generate(input.first, factorA).zip(generate(input.second, factorB))
+            .take(40_000_000)
+            .count { (a, b) ->
+                a.toShort().xor(b.toShort()) == 0.toShort()
+            }
+    }
+
+    fun part2(input: PosL): Int {
+        return generatePart2(input.first, factorA, 4).zip(generatePart2(input.second, factorB, 8))
+            .take(5_000_000)
+            .count { (a, b) ->
+                a.toShort().xor(b.toShort()) == 0.toShort()
+            }
     }
 }
 
 fun main() {
-    val testInput = """
-    """.trimIndent().split("\n")
+    val testInput = 65L to 8921L
     println("------Tests------")
     println(Day15.part1(testInput))
     println(Day15.part2(testInput))
 
     println("------Real------")
-    val input = readInput(2017, 15)
+    val input = 116L to 299L
     println("Part 1 result: ${Day15.part1(input)}")
     println("Part 2 result: ${Day15.part2(input)}")
     timingStatistics { Day15.part1(input) }
