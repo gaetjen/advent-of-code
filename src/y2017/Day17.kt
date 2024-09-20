1a5222b6c47fa6678278d83d3929dd93df0d537f
Johannes Gätjen <johannesgaetjen@gmail.com>
2024-09-20 22:09:58 +0200

    2017 day 17

--- src/y2017/Day17.kt
+++ src/y2017/Day17.kt
@@ -1,33 +1,45 @@
 package y2017
 
-import util.readInput
 import util.timingStatistics
 
 object Day17 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+
+
+    fun part1(input: Int): Int {
+        val buffer = mutableListOf(0)
+        var iteration = 0
+        var currentPosition = 0
+        while (iteration < 2017) {
+            currentPosition = (currentPosition + input) % buffer.size + 1
+            buffer.add(currentPosition, iteration + 1)
+            iteration++
+        }
+        return buffer[currentPosition + 1]
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
-    }
-
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part2(input: Int): Int {
+        val buffer = mutableListOf(0)
+        var iteration = 0
+        var currentPosition = 0
+        while (iteration < 50_000_000) {
+            currentPosition = (currentPosition + input) % (iteration + 1) + 1
+            if (currentPosition == 1) {
+                buffer.add(currentPosition, iteration + 1)
+            }
+            iteration++
+        }
+        return buffer[buffer.indexOf(0) + 1]
     }
 }
 
 fun main() {
-    val testInput = """
-    """.trimIndent().split("\n")
+    val testInput = 3
     println("------Tests------")
     println(Day17.part1(testInput))
     println(Day17.part2(testInput))
 
     println("------Real------")
-    val input = readInput(2017, 17)
+    val input = 376
     println("Part 1 result: ${Day17.part1(input)}")
     println("Part 2 result: ${Day17.part2(input)}")
     timingStatistics { Day17.part1(input) }
