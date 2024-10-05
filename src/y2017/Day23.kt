847c22ddd11c346e0f9d0ea5703bb88f31211506
Johannes Gätjen <johannesgaetjen@gmail.com>
2024-10-05 20:45:34 +0200

    2017 day 24

--- src/y2017/Day23.kt
+++ src/y2017/Day23.kt
@@ -2,6 +2,7 @@ package y2017
 
 import util.readInput
 import util.timingStatistics
+import y2017.Day23.program
 
 object Day23 {
     fun part1(input: List<String>): Number {
@@ -13,6 +14,71 @@ object Day23 {
             }
         }.last()
     }
+    fun program(log: Boolean): Long {
+        /**
+         * INITIALIZATION
+         * set b 84
+         * set c b
+         * jnz a 2
+         * jnz 1 5
+         * mul b 100
+         * sub b -100000
+         * set c b
+         * sub c -17000
+         */
+        var b = 108_400L
+        val c = 125_400L
+        var d: Long
+        var h = 0L
+
+        while (true) {
+            var f = true // A: set f 1
+            d = 2 // set d 2
+            do { // B: set e 2
+                if (b % d == 0L) {
+                    /**
+                     * C: set g d
+                     * mul g e
+                     * sub g b
+                     * jnz g 2
+                     * set f 0
+                     * sub e -1
+                     * set g e
+                     * sub g b
+                     * jnz g -8 GOTO C
+                     */
+                    f = false
+                }
+                d++ // sub d -1
+                /**
+                 * set g d
+                 * sub g b
+                 * jnz g -13 GOTO B
+                 */
+            } while (d != b)
+            /**
+             * jnz f 2
+             * sub h -1
+             */
+            if (!f) {
+                if (log) println("incrementing h: $h, b=$b, c=$c, d=$d")
+                h++
+            } else {
+                if (log) println("skipped incrementing h: $h, b=$b, c=$c, d=$d")
+            }
+            /**
+             * set g b
+             * sub g c
+             * jnz g 2
+             * jnz 1 3 BREAK
+             */
+            if (b == c) {
+                break
+            }
+            b += 17 // sub b -17
+        } //jnz 1 -23 GOTO A
+        return h
+    }
 }
 
 fun main() {
@@ -22,70 +88,4 @@ fun main() {
     println("Part 2 result: ${program(false)}")
     timingStatistics { Day23.part1(input) }
     timingStatistics { program(false) }
-}
-
-fun program(log: Boolean): Long {
-    /**
-     * INITIALIZATION
-     * set b 84
-     * set c b
-     * jnz a 2
-     * jnz 1 5
-     * mul b 100
-     * sub b -100000
-     * set c b
-     * sub c -17000
-     */
-    var b = 108_400L
-    val c = 125_400L
-    var d: Long
-    var h = 0L
-
-    while (true) {
-        var f = true // A: set f 1
-        d = 2 // set d 2
-        do { // B
-            if (b % d == 0L) {
-                /**
-                 * C: set g d
-                 * mul g e
-                 * sub g b
-                 * jnz g 2
-                 * set f 0
-                 * sub e -1
-                 * set g e
-                 * sub g b
-                 * jnz g -8 GOTO C
-                 */
-                f = false
-            }
-            d++ // sub d -1
-            /**
-             * set g d
-             * sub g b
-             * jnz g -13 GOTO B
-             */
-        } while (d != b)
-        /**
-         * jnz f 2
-         * sub h -1
-         */
-        if (!f) {
-            if (log) println("incrementing h: $h, b=$b, c=$c, d=$d")
-            h++
-        } else {
-            if (log) println("skipped incrementing h: $h, b=$b, c=$c, d=$d")
-        }
-        /**
-         * set g b
-         * sub g c
-         * jnz g 2
-         * jnz 1 3 BREAK
-         */
-        if (b == c) {
-            break
-        }
-        b += 17 // sub b -17
-    } //jnz 1 -23 GOTO A
-    return h
 }
--- src/y2017/Day24.kt
+++ src/y2017/Day24.kt
@@ -1,26 +1,64 @@
 package y2017
 
+import util.Pos
 import util.readInput
 import util.timingStatistics
 
+data class Bridge(
+    val lastPort: Int,
+    val components: List<Pair<Int, Int>>
+)
+
 object Day24 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<Pair<Int, Int>> {
+        return input.map { component ->
+            val (p1, p2) = component.split("/").map { it.toInt() }
+            p1 to p2
+        }
     }
 
-    fun part1(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    fun part1(input: List<String>): Int {
+        val components = parse(input).toSet()
+        val bridges = bridge(0, components)
+        val strongest = bridges.maxBy { bridge -> bridge.sumOf { it.first + it.second } }
+        return strongest.sumOf { it.first + it.second }
     }
 
-    fun part2(input: List<String>): Long {
-        val parsed = parse(input)
-        return 0L
+    private fun bridge(lastPort: Int, remainingComponents: Set<Pair<Int, Int>>): List<List<Pos>> {
+        return remainingComponents
+            .filter { it.first == lastPort || it.second == lastPort }
+            .flatMap { (p1, p2) ->
+                val nextPort = if (p1 == lastPort) p2 else p1
+                val tails = bridge(nextPort, remainingComponents - (p1 to p2))
+                if (tails.isEmpty()) {
+                    listOf(listOf(p1 to p2))
+                } else {
+                    tails.map { listOf(p1 to p2) + it }
+                }
+            }
+
+    }
+
+    fun part2(input: List<String>): Int {
+        val components = parse(input).toSet()
+        val bridges = bridge(0, components)
+        val longestLength = bridges.maxOf { it.size }
+        val longestBridges = bridges.filter { it.size == longestLength }
+        val strongest = longestBridges.maxBy { bridge -> bridge.sumOf { it.first + it.second } }
+        return strongest.sumOf { it.first + it.second }
     }
 }
 
 fun main() {
     val testInput = """
+        0/2
+        2/2
+        2/3
+        3/4
+        3/5
+        0/1
+        10/1
+        9/10
     """.trimIndent().split("\n")
     println("------Tests------")
     println(Day24.part1(testInput))
