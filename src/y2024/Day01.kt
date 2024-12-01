0c457f3e20600aade62bcd25a0a1814ba686e95a
Johannes Gätjen <johannesgaetjen@gmail.com>
2024-12-01 06:13:07 +0100

    2024 day 1

--- src/y2024/Day01.kt
+++ src/y2024/Day01.kt
@@ -2,26 +2,34 @@ package y2024
 
 import util.readInput
 import util.timingStatistics
+import util.transpose
+import kotlin.math.abs
 
 object Day01 {
-    private fun parse(input: List<String>): Any {
-        TODO()
+    private fun parse(input: List<String>): List<List<Int>> {
+        return input.map { line -> line.split(Regex("\\s+")).map { it.toInt() } }.transpose()
     }
 
     fun part1(input: List<String>): Int {
-        val parsed = parse(input)
-        return 0
+        val (l1, l2) = parse(input)
+        return l1.sorted().zip(l2.sorted()).sumOf { (a, b) -> abs(a - b) }
     }
 
     fun part2(input: List<String>): Int {
-        val parsed = parse(input)
-        return 0
+        val (l1, l2) = parse(input)
+        return l1.sumOf { a -> l2.count { it == a } * a }
     }
 }
 
 fun main() {
     val testInput = """
-    """.trimIndent().split("\n")
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+        """.trimIndent().split("\n")
     println("------Tests------")
     println(Day01.part1(testInput))
     println(Day01.part2(testInput))
